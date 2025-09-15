d0e114a7142df98820594ae731bb0eec6d50dc1e
Bexruz Developer <bexruzpy@gmail.com>
2025-09-15 12:43:29 +0500

    allow all hosts

--- Web/config/settings.py
+++ Web/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ww7myj6fnl43*(1&t04(#hdl07a56-1^qu30g9htt3uzm0nrbe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
